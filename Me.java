924b82cee80447a4954eaade197a397a06ade1ad
Charbel Jbeily <charbeljbeily17@gmail.com>
2023-01-23 20:09:22 +0200

    My year of study added

--- Me.java
+++ Me.java
@@ -2,5 +2,6 @@ public class Me {
 	public static void main (String [] args) {
 		System.out.println("My name is Charbel Jbeily");	
 		System.out.println("My major is mechatronics engineering");
+		System.out.println("I am a first year student");
 	}
 }
